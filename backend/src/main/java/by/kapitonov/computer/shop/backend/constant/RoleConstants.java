b7907afa3cbfb169e38bb923e04cd1e20dcf21e8
DrodiDui <boris.kapitobov.99@gmail.com>
2020-10-14 21:43:39 +0300

    Configuration files for security and webmvc has been added; add constants;

---
+++ backend/src/main/java/by/kapitonov/computer/shop/backend/constant/RoleConstants.java
@@ -0,0 +1,6 @@
+package by.kapitonov.computer.shop.backend.constant;
+
+public class RoleConstants {
+    public static final String USER = "user";
+    public static final String ADMIN = "admin";
+}
---
+++ backend/src/main/java/by/kapitonov/computer/shop/backend/constant/UserStatusConstants.java
@@ -0,0 +1,7 @@
+package by.kapitonov.computer.shop.backend.constant;
+
+public class UserStatusConstants {
+    public static final String ACTIVE = "active";
+    public static final String INACTIVE = "inactive";
+    public static final String DELETED = "deleted";
+}
